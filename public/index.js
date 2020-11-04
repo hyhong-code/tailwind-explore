ac9af614cdb49337220fffdd99347f14f9f62aac
Haiyang Hong <hong961127@gmail.com>
2020-11-04 12:19:08 -0800

    Toggle hidden class name on small screen

---
+++ public/index.js
@@ -0,0 +1,6 @@
+(function () {
+  // Hide or show navigation menu upon clicking burger icon
+  document.querySelector("#burger").addEventListener("click", function () {
+    document.querySelector("#menu").classList.toggle("hidden");
+  });
+})();
